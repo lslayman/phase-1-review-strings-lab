e06617e81abf2371a7d60e268c5369dacd893f5a
Lauren Slayman <laurenaslayman@gmail.com>
2023-03-19 17:24:30 -0600

    passes tests

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Lauren Slayman';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
